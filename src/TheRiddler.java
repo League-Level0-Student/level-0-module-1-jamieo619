942b0bd5fdf36cf51a7404f342eec8445f261c27
307304 <307304@0338S-S-R0B6H0K.sdcs.local>
2018-10-23 14:24:21 -0700

    hello

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -2,19 +2,29 @@ import javax.swing.JOptionPane;
 
 public class TheRiddler {
 public static void main(String[] args) {
-	
+	int score = 0;
 	JOptionPane.showMessageDialog(null,"GUESS THE RIDDLES!!!");
 	JOptionPane.showMessageDialog(null,"P.S. write the answer in all lowercase letters");
 	String coin = JOptionPane.showInputDialog("What has a head and a tail, but no body?");
 	if(coin.equals("coin")) {
 		JOptionPane.showMessageDialog(null,"CORRECT!!!");
-	
+	score = score+1;
+	JOptionPane.showMessageDialog(null, "Score: "+ score);
 	}
 	else {
 		JOptionPane.showMessageDialog(null,"WRONG!!!");
 		JOptionPane.showMessageDialog(null,"IT WAS A COIN!!!");
 	}
-	
+	String mushroom = JOptionPane.showInputDialog("What kind of room has no doors or windows?");
+	if(mushroom.equals("mushroom")) {
+		JOptionPane.showMessageDialog(null,"CORRECT!!!");
+		score = score+1;
+		JOptionPane.showMessageDialog(null, "Score: " + score);
+	}
+	else {
+		JOptionPane.showMessageDialog(null,"Wrong");
+		JOptionPane.showMessageDialog(null,"It was a mushroom");
+	}
 }
 
 
